cad1e450d97af6e2344791963a89242d7786bbb3
mikuto <mikuto1227k@gmail.com>
2025-04-04 17:04:32 +0000

    add:firebase初期化の設定

--- api/internal/app/app.go
+++ api/internal/app/app.go
@@ -1,12 +1,26 @@
 package app
 
 import (
+	"context"
 	"fmt"
-	
+	"log"
+
+	firebase "firebase.google.com/go"
+
 	"github.com/mikuto0831/camp2025_vol2/config"
+	"google.golang.org/api/option"
 )
 
 // run
-func Run(cfg *config.Config){
+func Run(cfg *config.Config) {
 	fmt.Println("App Name:", cfg.App.Name)
+
+	// Initialize Firebase
+	opt := option.WithCredentialsFile("firebase-adminsdk.json")
+	_, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Fatalf("error initializing app: %v", err)
+	} else {
+		fmt.Println("successfully initialized firebase app")
+	}
 }
